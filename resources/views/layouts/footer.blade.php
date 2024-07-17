87beb49f8b95e9be7998967ab3276c76516d6457
Lê Minh Đức <leminhduc5732@gmail.com>
2024-07-18 01:20:13 +0700

    create-layout

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,62 @@
+<div id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-6 col-12">
+                <p>
+                    <img src="{{assets('images/footer/footer_phone.webp')}}" alt="">Đặt hàng: 1800 6936
+                </p>
+                <p>
+                    <img src="{{assets('images/footer/footer_vitri.webp')}}" alt="">Liên hệ
+                </p>
+                <p>
+                    Tầng 3-4 Hub Building 195/10E Điện Biên Phủ, P.15 , Q.Bình Thạnh, TP.Hồ Chí Minh
+                </p>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12">
+                <div class="info">
+                    <a href="./sinhvien.html">
+                        <img class="logo" src="{{assets('images/footer/logo.jpg')}}" alt="">
+                        <img class="img" src="{{assets('images/footer/thecoffeehouse.jpg')}}" alt="">
+                    </a>
+                </div>
+                <div>
+                    <a href="">
+                        <img class="icon" src="{{assets('images/footer/icon-ig.svg')}}" alt="">
+                    </a>
+                    <a href="https://www.facebook.com/The.Coffee.House.2014/">
+                        <img class="icon" src="{{assets('images/footer/icon-facebook.svg')}}" alt="">
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-6">
+                <h3>Giới thiệu</h3>
+                <ul>
+                    <li><a href="./sinhvien.html">Về Chúng Tôi</a></li>
+                    <li><a href="./menu.html">Sản phẩm</a></li>
+                    <li><a href="./menu.html">Khuyến mãi</a></li>
+                    <li><a href="./sinhvien.html">Chuyện cà phê</a></li>
+                    <li><a href="./sinhvien.html">Cửa Hàng</a></li>
+                    <li><a href="./sinhvien.html">Tuyển dụng</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6 col-6">
+                <h3>Điều khoản</h3>
+                <ul>
+                    <li><a href="./sinhvien.html">Điều khoản sử dụng</a></li>
+                    <li><a href="./sinhvien.html">Chính sách bảo mật thông tin</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-12 col-md-12 col-12">
+                <div class="info-text">
+                    <p>Công ty cổ phần thương mại dịch vụ Trà Cà Phê VN</p>
+                    <p>Mã số DN: 0312867172 do sở kế hoạch và đầu tư tp. HCM cấp ngày 23/07/2014. Người đại diện: NGÔ
+                        NGUYÊN KHA</p>
+                    <p>Địa chỉ: 86-88 Cao Thắng, phường 04, quận 3, tp Hồ Chí Minh Điện thoại: (028) 7107 8079 Email:
+                        hi@thecoffeehouse.vn</p>
+                    <p>© 2014-2022 Công ty cổ phần thương mại dịch vụ Trà Cà Phê VN mọi quyền bảo lưu</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
