bb32d38ba40cb138a7fe1d721f7d70b135a3e8b8
Nguyễn Văn Hoàng Tâm <nguyenvanhoangtam2556@gmail.com>
2024-07-17 17:07:30 +0700

    success base

---
+++ app/Models/DetailCartTopping.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DetailCartTopping extends Model
+{
+    use HasFactory;
+    protected $table='detail_cart_toppings';
+    protected $fillable=[
+        'id',
+        'topping_id',
+        'cart_id'
+    ];
+}
---
+++ app/Models/DetailToppingProduct.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DetailToppingProduct extends Model
+{
+    use HasFactory;
+    protected $table='detail_topping_products';
+    protected $fillable=[
+        'id',
+        'cart_id',
+        'topping_id'
+    ];
+}
