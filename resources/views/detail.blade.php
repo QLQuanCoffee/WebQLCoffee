2a937b7e794f0b9da585b548452680bb68a62bed
Nguyễn Văn Hoàng Tâm <nguyenvanhoangtam2556@gmail.com>
2024-07-26 22:58:26 +0700

    handle product and detail

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,35 +1,5 @@
 @extends('layouts.app')
 @section('content')
-<<<<<<< Updated upstream
-    <div class="container">
-        <div class="row">
-            <div class="col-6">
-                <img class="w-100"
-                    src="https://product.hstatic.net/1000075078/product/1696220139_tra-xanh-espresso-marble_26a0eb92bfd649508d0e93173e9b3083.jpg"
-                    alt="">
-            </div>
-            <div class="col-6">
-                <h1>Trà xanh Espresso Marble</h1>
-                <h2>49.000đ</h2>
-                <br>
-                <p style="font-size: 1.5rem">Chọn size(bắt buộc)</p>
-                <div class="row option-size">
-                    <div class="d-flex align-items-center p-2 m-2 size">
-                        <img src="{{ asset('images/icon/glass_coffee.svg') }}" style="max-height: 15px; max-width: 15px;"
-                            alt="">
-                        <p class="mb-0 ms-2">Nhỏ + 0đ</p>
-                    </div>
-                    <div class="d-flex align-items-center p-2 m-2 size">
-                        <img src="{{ asset('images/icon/glass_coffee.svg') }}" style="max-height: 20px; max-width: 20px;"
-                            alt="">
-                        <p class="mb-0 ms-2">Vừa + 6000đ</p>
-                    </div>
-                    <div class="d-flex align-items-center p-2 m-2 size">
-                        <img src="{{ asset('images/icon/glass_coffee.svg') }}" style="max-height: 30px; max-width: 30px;"
-                            alt="">
-                        <p class="mb-0 ms-2">Lớn + 10000đ</p>
-                    </div>
-=======
     <div id="main-info">
         <div class="header-main">
             <div class="container">
@@ -58,19 +28,23 @@
                     <div class="col-lg-6 col-md-12 col-12">
                         <h2>{{ $product->name }}</h2>
                         <h2 class="money">{{ $product->price_format($product->price) }}</h2>
-                        <p class="title">Chọn size (bắt buộc)</p>
-                        <div class="option-size">
-                            <div class="size-item active">
-                                <i class="fas fa-cart-plus"></i>
-                                Nhỏ + 0 đ
+                        <br>
+                        <p style="font-size: 1.5rem">Chọn size(bắt buộc)</p>
+                        <div class="row option-size">
+                            <div class="d-flex align-items-center p-2 m-2 size">
+                                <img src="{{ asset('images/icon/glass_coffee.svg') }}" style="max-height: 15px; max-width: 15px;"
+                                    alt="">
+                                <p class="mb-0 ms-2">Nhỏ + 0đ</p>
                             </div>
-                            <div class="size-item">
-                                <i class="fas fa-cart-plus"></i>
-                                Vừa + 4.000 đ
+                            <div class="d-flex align-items-center p-2 m-2 size">
+                                <img src="{{ asset('images/icon/glass_coffee.svg') }}" style="max-height: 20px; max-width: 20px;"
+                                    alt="">
+                                <p class="mb-0 ms-2">Vừa + 6000đ</p>
                             </div>
-                            <div class="size-item">
-                                <i class="fas fa-cart-plus"></i>
-                                Lớn + 10.000 đ
+                            <div class="d-flex align-items-center p-2 m-2 size">
+                                <img src="{{ asset('images/icon/glass_coffee.svg') }}" style="max-height: 30px; max-width: 30px;"
+                                    alt="">
+                                <p class="mb-0 ms-2">Lớn + 10000đ</p>
                             </div>
                         </div>
                         <p class="title">Topping</p>
